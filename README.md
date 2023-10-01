45916c98d636f5cc763aafc22b9af037da41b217
Ivy-Walobwa <ivywalobwa@gmail.com>
2023-10-01 21:47:56 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Dynamic Shiny.React App
+
+A simple example of a dynamically styled react app using shiny.react
+
+- The select input is a shiny input control
+- The container with the background color is a react component
+- Data from the shiny input control is passed into React component as props
+- The input value is observed and the React component rerenders whenever it changes 
+
+<img width="1440" alt="Screenshot 2023-10-01 at 21 44 08" src="https://github.com/Ivy-Walobwa/responsive-pure-css-menu/assets/49009293/dea228de-76cf-4aaa-85f5-c5347eaa103f">
---
+++ app.R
@@ -0,0 +1,74 @@
+library(shiny.react)
+
+colors <- list("Gold", "Lavender", "Salmon")
+selectedColor <- reactiveVal("Gold")
+
+CustomComponents <- tags$script(HTML("(function() {
+  const React = jsmodule['react'];
+  const Shiny = jsmodule['@/shiny'];
+  const CustomComponents = jsmodule['CustomComponents'] ??= {};
+
+  CustomComponents.ReactComponent = function(props) {
+    const {value} = props;
+    return React.createElement('div', {className:'nav', style:{backgroundColor: value, height:'120px', padding:'20px'}},`You have selected the color ${value}.`)
+  };
+})()"))
+
+ReactComponent <- function(...) {
+  shiny.react::reactElement(
+    module = "CustomComponents",
+    name = "ReactComponent",
+    props = shiny.react::asProps(...),
+  )
+}
+
+ui<- function(id) {
+  ns <- NS(id)
+   bootstrapPage(
+    tagList(CustomComponents),
+    navbarPage(
+      title = "Dynamic Shiny.React App",
+      windowTitle = "Dynamic Shiny React App",
+    ),
+    div(
+      class = "container",
+      fluidRow(
+          column(
+              width = 3,
+              wellPanel(
+                selectInput(ns("color"), label = "Background color", choices = colors),
+             )
+          ),
+          column(
+              width = 9,
+              div(
+                  reactOutput(ns("reactComponent"))
+              )
+          )
+      )
+    )
+)
+}
+
+
+
+server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    selectedColor <- reactiveVal("Gold")
+    rebuildReactComponent <- function() {
+      output$reactComponent<- renderReact({
+          ReactComponent( value = selectedColor())
+       })
+    }
+    observeEvent(input$color, {
+        selectedColor(input$color)
+        rebuildReactComponent()
+    })
+    rebuildReactComponent()
+  })
+
+}
+
+if (interactive()) {
+  shinyApp(ui("app"), function(input, output) server("app"))
+}
